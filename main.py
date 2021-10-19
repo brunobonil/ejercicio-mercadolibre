00c8c9be363f7813a541d869327b87b581e8533b
brunobonil <brunobonil@gmail.com>
2021-10-18 21:51:11 -0300

    Arreglos

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import re
 
-adn = ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]
-adn_str = ' '.join(adn)
+def isMutant(adn):
+    adn_str = ' '.join(adn)
+    long = len(adn[0])
 
-long = len(adn[0])
+    horizontal = re.compile(r'^A{4}|T{4}|C{4}|G{4}$') # Busca coincidencias horizontales
+    vertical = re.compile(r'([ACTG])(.{' + re.escape(str(long)) + r'}\1){3}') # Busca coincidencias verticales
+    oblicuo_desc = re.compile(r'([ACTG])(.{' + re.escape(str(long + 1)) + r'}\1){3}') # Busca coincidencias en diagonal de izquierda a derecha
+    oblicuo_asc = re.compile(r'([ACTG])(.{' + re.escape(str(long - 1)) + r'}\1){3}') # Busca coincidencias en diagonal de derecha a izquierda
 
-horizontal = re.compile(r'^A{4}|T{4}|C{4}|G{4}$') 
+    # Creo una lista con las expresiones regulares
+    expr = [horizontal, vertical, oblicuo_desc, oblicuo_asc]
+    result = list()
+    
+    # Itero sobre la lista para determinar si encontraron una coincidencia
+    for i in expr:
+        buscar = i.finditer(adn_str)
 
-vertical = re.compile(r'([ACTG])(.{' + re.escape(str(long)) + r'}\1){3}') 
+    #En caso de encontrar una coincidencia se agrega un True a la lista
+        for i in buscar:
+            result.append((bool(i)))
 
-oblicuo_desc = re.compile(r'([ACTG])(.{' + re.escape(str(long + 1)) + r'}\1){3}')
 
-oblicuo_asc = re.compile(r'([ACTG])(.{' + re.escape(str(long - 1)) + r'}\1){3}')
+    # Se realiza un conteo de la cantidad de coincidencias que hubo
+    if result.count(True) > 1:
+        return True
+    else:
+        return False
 
-expr = [horizontal, vertical, oblicuo_desc, oblicuo_asc]
-result = list()
+if __name__ == '__main__':
 
-for i in expr:
-    result.append((bool(i.findall(adn_str))))
+    adn = ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]
 
-if result.count(True) > 1:
-    print('Es un mutante')
-
-else:
-    print('No es un mutante')
+    print(isMutant(adn))
+    
